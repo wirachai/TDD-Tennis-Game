134d2dc9b657c914eee1d111a828fb6ce157ccb3
wirachai <wirachai.me@gmail.com>
2015-11-27 10:32:03 +0700

    Submit solution

---
+++ TennisGame.Test/TennisGameTest.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+
+namespace TennisGame.Test
+{
+    [TestFixture]
+    internal class TennisGameTest
+    {
+        [Test]
+        public void GetScore_ShouldReturnLoveLove_WhenScoreIs0And0()
+        {
+            TennisGame game = new TennisGame(0, 0);
+            Assert.AreEqual("Love-Love", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnFifteenLove_WhenScoreIs1And0()
+        {
+            TennisGame game = new TennisGame(1, 0);
+            Assert.AreEqual("Fifteen-Love", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnThirtyLove_WhenScoreIs2And0()
+        {
+            TennisGame game = new TennisGame(2, 0);
+            Assert.AreEqual("Thirty-Love", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnFortyLove_WhenScoreIs3And0()
+        {
+            TennisGame game = new TennisGame(3, 0);
+            Assert.AreEqual("Forty-Love", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnLoveFifteen_WhenScoreIs0And1()
+        {
+            TennisGame game = new TennisGame(0, 1);
+            Assert.AreEqual("Love-Fifteen", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnThirtyThirty_WhenScoreIs2And2()
+        {
+            TennisGame game = new TennisGame(2, 2);
+            Assert.AreEqual("Thirty-Thirty", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnDeuce_WhenScoreIs3And3()
+        {
+            TennisGame game = new TennisGame(3, 3);
+            Assert.AreEqual("Deuce", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnDeuce_WhenScoreIs4And4()
+        {
+            TennisGame game = new TennisGame(4, 4);
+            Assert.AreEqual("Deuce", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnDeuce_WhenScoreIs6And6()
+        {
+            TennisGame game = new TennisGame(6, 6);
+            Assert.AreEqual("Deuce", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnWinPlayer1_WhenScoreIs4And2()
+        {
+            TennisGame game = new TennisGame(4, 2);
+            Assert.AreEqual("Win Player1", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnWinPlayer2_WhenScoreIs2And4()
+        {
+            TennisGame game = new TennisGame(2, 4);
+            Assert.AreEqual("Win Player2", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnAdvPlayer1_WhenScoreIs4And5()
+        {
+            TennisGame game = new TennisGame(4, 5);
+            Assert.AreEqual("Adv Player2", game.GetScore());
+        }
+
+        [Test]
+        public void GetScore_ShouldReturnAdvPlayer2_WhenScoreIs5And4()
+        {
+            TennisGame game = new TennisGame(5, 4);
+            Assert.AreEqual("Adv Player1", game.GetScore());
+        }
+
+        [Test]
+        public void RealisticTennisGame()
+        {
+            TennisGame game = new TennisGame(0, 0);
+            Assert.AreEqual("Love-Love", game.GetScore());
+
+            string[] points = { "player1", "player1", "player2", "player2", "player1", "player1" };
+            string[] expectedScores = { "Fifteen-Love", "Thirty-Love", "Thirty-Fifteen", "Thirty-Thirty", "Forty-Thirty", "Win Player1" };
+            for (int i = 0; i < expectedScores.Length; i++)
+            {
+                game.WonPoint(points[i]);
+                Assert.AreEqual(expectedScores[i], game.GetScore());
+            }
+        }
+    }
+}
---
+++ TennisGame/TennisGame.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace TennisGame
+{
+    public class TennisGame
+    {
+        private const int maxScore = 3;
+        private int player1Score;
+        private int player2Score;
+        
+        public TennisGame(int player1Score, int player2Score)
+        {
+            this.player1Score = player1Score;
+            this.player2Score = player2Score;
+        }
+
+        public string GetScore()
+        {
+            if (IsWin())
+            {
+                return "Win " + GetAdvantagePlayer();
+            }
+
+            if (IsAdvantageScore())
+            {
+                return "Adv " + GetAdvantagePlayer();
+            }
+
+            if (IsDeuce())
+            {
+                return "Deuce";
+            }
+
+            string[] scores = { "Love", "Fifteen", "Thirty", "Forty" };
+            return scores[player1Score] + "-" + scores[player2Score];
+        }
+
+        public void WonPoint(string playerName)
+        {
+            if (playerName.Equals("player1"))
+            {
+                player1Score++;
+            }
+            else
+            {
+                player2Score++;
+            }
+        }
+
+        private bool IsAdvantageScore()
+        {
+            int diffScore = GetDifferenceScore();
+            return (player1Score > maxScore || player2Score > maxScore)
+                    && diffScore == 1;
+        }
+
+        private bool IsWin()
+        {
+            int diffScore = GetDifferenceScore();
+            return (player1Score > maxScore || player2Score > maxScore)
+                    && diffScore >= 2;
+        }
+
+        private bool IsDeuce()
+        {
+            int diffScore = GetDifferenceScore();
+            return (player1Score >= maxScore && player2Score >= maxScore)
+                    && diffScore == 0;
+        }
+
+        private string GetAdvantagePlayer()
+        {
+            return (player1Score > player2Score ? "Player1" : "Player2");
+        }
+
+        private int GetDifferenceScore()
+        {
+            return Math.Abs(player1Score - player2Score);
+        }
+    }
+}
